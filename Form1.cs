c1dca5649e0ea2a68f9b69ab40b04390bc8d0b69
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2023-07-26 12:11:24 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 form2 = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "joselito" && textBox2.Text == "1234")
+            {
+                label3.Text = "Datos Correctos";
+                this.Hide();
+                form2.ShowDialog();
+                this.Show();
+            } else
+            {
+                label3.Text = "Datos Incorrectos";
+            }
+        }
+    }
+}
